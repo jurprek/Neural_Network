d7fb56a30e574fe11157002cd701a45a9add78f9
Jurica Preksavec <jurica.preksavec@omega-software.hr>
2023-03-15 10:47:26 +0100

    vscd

--- NrlNtwrk/Declare.cs
+++ NrlNtwrk/Declare.cs
@@ -1,15 +1,16 @@
 ﻿partial class NeuralNetwork
 {
     // Define Neural Network.
-    private static int INPUT = 10;
-    private static int Size1 = 20;
-    private static int Size2 = 20;
+    private static int INPUT = 5;
+    private static int Size1 = 6;
+    private static int Size2 = 4;
     private static int OUTPUT = 1;
-    private static int CycleNumber = 0;
+    private static int CycleNumber = 10;
 
     // Load data from .CSV & .txt files.
-    private static string DatafilePath = @"Data\DataSet01.csv";
+    private static string DatafilePath = @"Data\Titanic.csv";
     private static string weightsfilePath1 = @"Weights\weights1.txt";
     private static string weightsfilePath2 = @"Weights\weights2.txt";
     private static string weightsfilePath3 = @"Weights\weights3.txt";
+    private static string biasesFile = @"Weights\Biases.txt";
 }
