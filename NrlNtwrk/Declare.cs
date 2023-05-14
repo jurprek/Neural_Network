e9f6bc09c661e95b07a2cf518dcb53d537904160
jurprek <113016805+jurprek@users.noreply.github.com>
2023-05-15 00:41:41 +0200

    Titanik

--- NrlNtwrk/Declare.cs
+++ NrlNtwrk/Declare.cs
@@ -1,15 +1,15 @@
 ﻿partial class NeuralNetwork
 {
     // Define Neural Network.
-    private static int INPUT = 5;
+    private static int INPUT = 2;
     private static int Size1 = 17;
-    private static int Size2 = 14;
+    private static int Size2 = 13;
     private static int OUTPUT = 1;
-    private static int CycleNumber = 5000;
+    private static int CycleNumber = 10000;
     private static double Overfitting = 0.70;
 
     // Load data from .CSV & .txt files.
-    private static string DatafilePath = @"Data\Titanic.csv";
+    private static string DatafilePath = @"Data\TitanicData.csv";
     private static string weightsfilePath1 = @"Weights\weights1.txt";
     private static string weightsfilePath2 = @"Weights\weights2.txt";
     private static string weightsfilePath3 = @"Weights\weights3.txt";
